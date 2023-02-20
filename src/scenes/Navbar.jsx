dce071b12c7369bad701e105ab352aea290b36c6
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-20 20:55:27 +0100

    `Navbar` fixed all yellow elements and menu.svg

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -16,8 +16,9 @@ const Link = ({page, selectedPage, setSelectedPage}) => {
 
   return (
     <AnchorLink 
-      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
-        hover: text-yellow transition duration-500`}
+    className={`${
+      selectedPage === lowerCasePage ? "text-yellow" : ""
+    } hover:text-yellow transition duration-500`}
       href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
     >
