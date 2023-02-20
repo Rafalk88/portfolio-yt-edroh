1b2b152ba3721a4a46e1e11e99774e9c88cd187a
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-20 19:11:46 +0100

    `App` nav constants added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import React from "react";
+
+import Navbar from './scenes/Navbar'
+import { useMediaQuery } from './hooks/useMediaQuery';
+
+const App = () => {
+  const [selectedPage, setSelectedPage] = React.useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <div className="app">
-      
+    <div className="app bg-deep-blue">
+      <Navbar 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
-}
+};
 
 export default App;
