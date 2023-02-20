4f82930a2d4c50e0282335fa66beda3512cb1a60
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-20 21:30:51 +0100

    `Navbar` adds change nav bg color after scroll event

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,26 @@ import { useMediaQuery } from './hooks/useMediaQuery';
 
 const App = () => {
   const [selectedPage, setSelectedPage] = React.useState('home');
+  const [isTopOfPage, setIsTopOfPage] = React.useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY === 0 ? setIsTopOfPage(true) : setIsTopOfPage(false)
+    }
+    window.addEventListener("scroll", handleScroll)
+
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar 
+      <Navbar
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <div className="w-5/6 mx-auto md:h-full"></div>
     </div>
   );
 };
