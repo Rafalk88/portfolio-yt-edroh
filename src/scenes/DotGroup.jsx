6e6584e17a05e876d5ba3870605a76f748d29aec
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-23 21:05:26 +0100

    `landing` created & `DotGroup` const moved outside fn

--- src/scenes/DotGroup.jsx
+++ src/scenes/DotGroup.jsx
@@ -4,12 +4,12 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { menu } from './Navbar'
 
 const dotMenu = menu
-
-const DotGroup = ({selectedPage, setSelectedPage}) => {
-  const selectedStyles = `relative bg-yellow before:absolute before:w-6 before:h-6
+const selectedStyles = `relative bg-yellow before:absolute before:w-6 before:h-6
     before:rounded-full before:border-2 before:border-yellow before:left-[-50%]
     before:top-[-50%]`;
 
+const DotGroup = ({selectedPage, setSelectedPage}) => {
+  
   return (
     <div className="flex flex-col gap-6 fixed top-[60%] right-7">
       {
