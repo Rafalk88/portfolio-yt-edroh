ce3f16f11ea040230dc033df5293198c520fe0ba
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-24 19:13:21 +0100

    `SocialMediaIcons` first icon added

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 export const SocialMediaIcons = () => {
   return (
-    <div>SCM component</div>
+    <section className="flex justify-center md:justify-start my-10 gap-7">
+      <a 
+        href="https://linkedin.com"
+        className="hover:opacity-50 transition duration-500"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/linkedin.png" alt="linkedin-link" />
+      </a>
+    </section>
   )
 };
 
