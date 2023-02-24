0f8d2c74d6ae61205ac63b9ae57539a453542c46
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-24 19:34:15 +0100

    `SocialMediaIcons` done

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 
+const icons = [
+  {name: 'linkedin', link: 'https://linkedin.com'},
+  {name: 'twitter', link: 'https://twitter.com'},
+  {name: 'facebook', link: 'https://facebook.com'},
+  {name: 'instagram', link: 'https://instagram.com'}
+]
+
 export const SocialMediaIcons = () => {
   return (
     <section className="flex justify-center md:justify-start my-10 gap-7">
-      <a 
-        href="https://linkedin.com"
-        className="hover:opacity-50 transition duration-500"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src="../assets/linkedin.png" alt="linkedin-link" />
-      </a>
+      {
+        icons.map((icon) => {
+          return (
+            <a 
+              href={icon.link}
+              className="hover:opacity-50 transition duration-500"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={`../assets/${icon.name}.png`} alt={`${icon.name}-link`} />
+            </a>
+          )
+        })
+      }
     </section>
   )
 };
