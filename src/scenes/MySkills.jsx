2183c0654792492b01f7de970053829d004aea13
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-24 21:08:45 +0100

    `MySkill` skills added

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -4,6 +4,12 @@ import { motion } from 'framer-motion';
 import LineGradient from '../components/LineGradient';
 import { useMediaQuery } from '../hooks/useMediaQuery';
 
+const skills = [
+  {id: '01', delay: 0, color: 'bg-blue', skill: 'Experience', text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt sed similique id perferendis aspernatur. Ex distinctio mollitia eligendi nam.'},
+  {id: '02', delay: 0.2, color: 'bg-red', skill: 'Innovative', text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt sed similique id perferendis aspernatur. Ex distinctio mollitia eligendi nam.'},
+  {id: '03', delay: 0.4, color: 'bg-yellow', skill: 'Imaginative', text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt sed similique id perferendis aspernatur. Ex distinctio mollitia eligendi nam.'},
+]
+
 export const MySkills = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
@@ -53,26 +59,31 @@ export const MySkills = () => {
       </article>
 
       <section className="md: flex md: justify-between mt-16 gap-32">
-        <article>
-          <motion.div
-          className="md:w-1/3 mt-10"
-          initial={{ opacity: 0, y: -50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-        >
-          <article className="relative h-32">
-            <div className="z-10">
-              <p className="font-playfair font-semibold text-5xl">01</p>
-              <p className="font-playfair font-semibold text-3xl mt-3">Experience</p>
-            </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
-          </article>
-          <p className="mt-5">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt sed similique id perferendis aspernatur. Ex distinctio mollitia eligendi nam.
-          </p>
-        </motion.div>
-        </article>
+        {
+          skills.map((skill) => {
+            return (
+              <motion.div
+                key={skill.id}
+                className="md:w-1/3 mt-10"
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: skill.delay, duration: 0.5 }}
+              >
+                <article className="relative h-32">
+                  <div className="z-10">
+                    <p className="font-playfair font-semibold text-5xl">{skill.id}</p>
+                    <p className="font-playfair font-semibold text-3xl mt-3">{skill.skill}</p>
+                  </div>
+                  <div className={`w-1/2 md:w-3/4 h-32 ${skill.color} absolute right-0 top-0 z-[-1]`} />
+                </article>
+                <p className="mt-5">
+                  {skill.text}
+                </p>
+              </motion.div>
+            )
+          })
+        }
       </section>
     </section>
   )
