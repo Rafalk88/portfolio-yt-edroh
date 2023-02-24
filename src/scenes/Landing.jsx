52ec5b1ea0b4700e93759b60a59548cfa17de648
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-24 20:00:28 +0100

    `Landing` fixed not working framer motion

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -10,7 +10,7 @@ export const Landing = ({setSelectedPage}) => {
 
   return (
     <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
-      <div className="md:order-2 flec justify-center basis-3/5 z-10 mt-16 md:mt-32">
+      <div className="md:order-2 flec justify-center basis-2/5 z-10 mt-16 md:mt-32">
         {isAboveMediumScreens ? (
           <div
             className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20
@@ -21,7 +21,7 @@ export const Landing = ({setSelectedPage}) => {
               src="../assets/profile-image.png"
               alt="profile"
               className="hover: filter hover:saturate-200 transition duration-500 z-10 w-full
-              max-w-[400px] md:max-w-[600px]"
+              max-w-[300px] md:max-w-[400px]"
             />
           </div>
         ) : (
@@ -29,20 +29,16 @@ export const Landing = ({setSelectedPage}) => {
               src="../assets/profile-image.png"
               alt="profile"
               className="hover: filter hover:saturate-200 transition duration-500 z-10 w-full
-              max-w-[400px] md:max-w-[600px]"
+              max-w-[300px] md:max-w-[400px]"
             />
         )}
       </div>
       <div className="z-30 basis-2/5 mt-12 md:mt-32">
         <motion.div
-          initial="hidden"
-          whileInView="visible"
+          initial={{opacity: 0, x: -50}}
+          whileInView={{opacity: 1, x: 0}}
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variant={{ 
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 }
-          }}
+          ransition={{ duration: 0.5 }}
         >
           <p className="text-6xl font-playfair z-10 text-center md:text-start">
             Jane {""}
@@ -61,14 +57,10 @@ export const Landing = ({setSelectedPage}) => {
 
         <motion.div
           className="flex mt-5 justify-center md:justify-start"
-          initial="hidden"
-          whileInView="visible"
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true, amount: 0.5 }}
           transition={{ delay: 0.2, duration: 0.5 }}
-          variant={{ 
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 }
-          }}
         >
           <AnchorLink
             className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
@@ -95,14 +87,10 @@ export const Landing = ({setSelectedPage}) => {
 
         <motion.div
           className="flex mt-5 justify-center md:justify-start"
-          initial="hidden"
-          whileInView="visible"
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true, amount: 0.5 }}
           transition={{ delay: 0.4, duration: 0.5 }}
-          variant={{ 
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 }
-          }}
         >
           <SocialMediaIcons />
         </motion.div>
