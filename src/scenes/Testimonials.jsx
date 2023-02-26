7eca481f5c0f1e2c8c95fd9fb8e10506435f9485
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-26 12:08:34 +0100

    `Testimonials` refactored

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -6,6 +6,12 @@ const testimonialsStyles = `mx-auto relative max-w-[400px] h-[350px]
   flex flex-col justify-end p-16 mt-48 before:absolute before:top-[-120px] 
   before:-ml-[110px] before:left-1/2`;
 
+const testimonials = [
+  {id: 1, bgColor: 'bg-blue', content: 'person1', delay: 0, text: 'Lorem, ipsum dolor sit amet consectetur'},
+  {id: 2, bgColor: 'bg-red', content: 'person2', delay: 0.2, text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'},
+  {id: 3, bgColor: 'bg-yellow', content: 'person3', delay: 0.4, text: 'Lorem, ipsum dolor sit amet consectetur adipisicing'},
+]
+
 export const Testimonials = () => {
   return (
     <section id="testimonials" className="pt-32 pb-16">
@@ -28,44 +34,25 @@ export const Testimonials = () => {
         </motion.div>
 
         <div className="md:flex md:justify-between gap-8">
-          <motion.div
-            className={`bg-blue ${testimonialsStyles} before:content-person1`}
-            initial={{ opacity: 0, scale: 0.8 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ duration: 0.5 }}
-          >
-            <p className="font-playfair text-6xl">"</p>
-            <p className="text-center text-xl">
-              Lorem, ipsum dolor sit amet consectetur
-            </p>
-          </motion.div>
-
-          <motion.div
-            className={`bg-red ${testimonialsStyles} before:content-person2`}
-            initial={{ opacity: 0, scale: 0.8 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.2, duration: 0.5 }}
-          >
-            <p className="font-playfair text-6xl">"</p>
-            <p className="text-center text-xl">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </motion.div>
-
-          <motion.div
-            className={`bg-yellow ${testimonialsStyles} before:content-person3`}
-            initial={{ opacity: 0, scale: 0.8 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.4, duration: 0.5 }}
-          >
-            <p className="font-playfair text-6xl">"</p>
-            <p className="text-center text-xl">
-              Lorem, ipsum dolor sit amet consectetur adipisicing
-            </p>
-          </motion.div>
+          {
+            testimonials.map(testimonial => {
+              return (
+                <motion.div
+                  key={testimonial.id}
+                  className={`${testimonial.bgColor} ${testimonialsStyles} before:content-${testimonial.content}`}
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  whileInView={{ opacity: 1, scale: 1 }}
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ delay: testimonial.delay, duration: 0.5 }}
+                >
+                  <p className="font-playfair text-6xl">"</p>
+                  <p className="text-center text-xl">
+                    {testimonial.text}
+                  </p>
+                </motion.div>
+              )
+            })
+          }
         </div>
     </section>
   )
