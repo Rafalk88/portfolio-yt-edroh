2bddd8ffcd1052b99cc6f596345347599ac382bc
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-27 21:26:41 +0100

    `ProjectInfo` added and basic logic done

--- src/scenes/Project.jsx
+++ src/scenes/Project.jsx
@@ -1,23 +1,34 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
 
+import ProjectInfo from './ProjectInfo';
+
 const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue cursor-pointer`;
 
 export const Project = ({ title = '', projectVariant = {}, content = '' }) => {
+  const [isInfoShown, setIsInfoShown] = React.useState(false);
+
   const projectTitle = title.split(' ').join('-').toLowerCase();
   
   return (
-    <motion.div variants={projectVariant} className="relative">
+    <motion.div variants={projectVariant} className="relative" onClick={() => { setIsInfoShown(!isInfoShown) }}>
       <article className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
         <p className="mt-7">
           {content}
         </p>
       </article>
-        <figure>
-          <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
-        </figure>
+      <figure>
+        <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+      </figure>
+      {
+        isInfoShown ?
+        <ProjectInfo />
+        :
+        null
+      }
     </motion.div>
   )
 };
---
+++ src/scenes/ProjectInfo.jsx
@@ -0,0 +1,7 @@
+export const ProjectInfo = () => {
+  return (
+    <div>info component</div>
+  )
+};
+
+export default ProjectInfo;
