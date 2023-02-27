f5b632ab022ea748f28e1b3393c62b1816c65aa3
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-27 16:17:49 +0100

    `LineGradient` added proptypes

--- src/components/LineGradient.jsx
+++ src/components/LineGradient.jsx
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types';
+
 export const LineGradient = ({ width = "w-full" }) => {
   return (
     <div className={`h-0.5 ${width} bg-gradient-rainblue`} />
   )
 }
+
+LineGradient.propTypes = {
+  width: PropTypes.string,
+};
 
 export default LineGradient;
