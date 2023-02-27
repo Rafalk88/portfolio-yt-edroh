9fe9d08b5e95486796a0565176f30f5ce5eabf2b
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-27 18:55:51 +0100

    `Navbar` refactored and changes `setMenuItems` to toggle menu after click

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
+import Link from '../components/Link'
 import useMediaQuery from '../hooks/useMediaQuery';
 
 export const menu = [
@@ -11,36 +13,30 @@ export const menu = [
   {id: 5, page: "Contact"},
 ]
 
-const Link = ({page, selectedPage, setSelectedPage}) => {
-  const lowerCasePage = page.toLowerCase();
-
-  return (
-    <AnchorLink 
-    className={`${
-      selectedPage === lowerCasePage ? "text-yellow" : ""
-    } hover:text-yellow transition duration-500`}
-      href={`#${lowerCasePage}`}
-      onClick={() => setSelectedPage(lowerCasePage)}
-    >
-      {page}
-    </AnchorLink>
-  )
-};
-
 const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}) => {
   const [isMenuToggled, setIsMenuToggled] = React.useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
   const navbarBackground = isTopOfPage ? "" : "bg-red";
 
-  const setMenuItems = () => {
+  const setMenuItems = (screen) => {
     return (
       menu.map((item) => {
         return (
+          screen === 'big' ? 
           <Link
             key={item.id}
             page={item.page}
             selectedPage={selectedPage}
             setSelectedPage={setSelectedPage}
+          />
+          : 
+          <Link
+            key={item.id}
+            page={item.page}
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+            isMenuToggled={isMenuToggled}
+            setIsMenuToggled={setIsMenuToggled}
           />
         )
       })
@@ -49,11 +45,11 @@ const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}) => {
 
   return (
     <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
-      <div className="flex items-center justify-between mx-auto w-5/6">
+      <section className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">
           <AnchorLink
             href="#home"
-            onClick={() => setSelectedPage("#home")}
+            onClick={() => setSelectedPage("home")}
           >
             JE
           </AnchorLink>
@@ -61,7 +57,7 @@ const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}) => {
 
         {isAboveSmallScreens ? (
           <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
-            { setMenuItems() }
+            { setMenuItems("big") }
           </div>
         ) : (
           <button
@@ -73,7 +69,7 @@ const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}) => {
         ) }
 
         {!isAboveSmallScreens && isMenuToggled && (
-          <div className=" fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+          <section className=" fixed right-0 bottom-0 h-full bg-blue w-[300px]">
             <div className="flex justify-end p-12">
               <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
                 <img src="../assets/close-icon.svg" alt="close-icon" />
@@ -81,13 +77,19 @@ const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}) => {
             </div>
 
             <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
-            { setMenuItems() }
+            { setMenuItems("small") }
             </div>
-          </div>
+          </section>
         )}
-      </div>
+      </section>
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  isTopOfPage: PropTypes.bool.isRequired,
+  selectedPage: PropTypes.string.isRequired,
+  setSelectedPage: PropTypes.func.isRequired,
 };
 
 export default Navbar;
