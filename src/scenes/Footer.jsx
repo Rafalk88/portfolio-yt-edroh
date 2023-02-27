67951ae883993fb799df04325a6722b9ad92bc76
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-27 16:56:39 +0100

    `footer` refactored

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -4,13 +4,13 @@ import { icons } from '../scenes/Landing';
 export const Footer = () => {
   return (
     <footer className="h-64 bg-red pt-10">
-      <div className="w-5/6 mx-auto">
+      <section className="w-5/6 mx-auto">
         <SocialMediaIcons icons={icons}/>
-        <div className="md:flex justify-center md:justify-between text-center">
+        <article className="md:flex justify-center md:justify-between text-center">
           <p className="font-playfair font-semibold text-2xl text-yellow">JANE ESPER</p>
           <p className="font-playfair text-md text-yellow">©2023 ESPER. All Rights Reserved.</p>
-        </div>
-      </div>
+        </article>
+      </section>
     </footer>
   )
 };
