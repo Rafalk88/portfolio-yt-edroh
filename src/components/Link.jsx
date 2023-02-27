4e3c31fe33ab50c7385cbec2765e6f575683cf57
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-02-27 18:55:14 +0100

    moved `Link` to components

---
+++ src/components/Link.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+export const Link = ({page, selectedPage, setSelectedPage, isMenuToggled, setIsMenuToggled}) => {
+  const lowerCasePage = page.toLowerCase();
+
+  return (
+    <AnchorLink 
+    className={`${
+      selectedPage === lowerCasePage ? "text-yellow" : ""
+    } hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => {
+        setSelectedPage(lowerCasePage)
+        setIsMenuToggled(!isMenuToggled)
+      }}
+    >
+      {page}
+    </AnchorLink>
+  )
+};
+
+Link.propTypes = {
+  page: PropTypes.string.isRequired,
+  selectedPage: PropTypes.string.isRequired,
+  setSelectedPage: PropTypes.func.isRequired,
+  isMenuToggled: PropTypes.bool,
+  setIsMenuToggled: PropTypes.func,
+};
+
+export default Link
